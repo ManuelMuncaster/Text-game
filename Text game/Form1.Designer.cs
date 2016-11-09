3deb490502f8ddda55f7cbfd94c0f1f64ea7b255
ManuelMuncaster <russmunc061@ed.amdsb.ca>
2016-11-08 21:17:02 -0500

    Version #2

--- Text game/Form1.Designer.cs
+++ Text game/Form1.Designer.cs
@@ -28,12 +28,158 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.eventOutputlabel = new System.Windows.Forms.Label();
+            this.eventPicturebox = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.blueOutputLabel = new System.Windows.Forms.Label();
+            this.redOutputlabel = new System.Windows.Forms.Label();
+            this.yellowOutputlabel = new System.Windows.Forms.Label();
+            this.greenOutputlabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.eventPicturebox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // eventOutputlabel
+            // 
+            this.eventOutputlabel.Font = new System.Drawing.Font("Constantia", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.eventOutputlabel.ForeColor = System.Drawing.Color.White;
+            this.eventOutputlabel.Location = new System.Drawing.Point(12, 9);
+            this.eventOutputlabel.Name = "eventOutputlabel";
+            this.eventOutputlabel.Size = new System.Drawing.Size(456, 133);
+            this.eventOutputlabel.TabIndex = 0;
+            this.eventOutputlabel.Text = "label1";
+            // 
+            // eventPicturebox
+            // 
+            this.eventPicturebox.Location = new System.Drawing.Point(12, 145);
+            this.eventPicturebox.Name = "eventPicturebox";
+            this.eventPicturebox.Size = new System.Drawing.Size(456, 148);
+            this.eventPicturebox.TabIndex = 1;
+            this.eventPicturebox.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Text_game.Properties.Resources.blue_50x50;
+            this.pictureBox2.Location = new System.Drawing.Point(12, 317);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(54, 50);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::Text_game.Properties.Resources.red_50x50;
+            this.pictureBox3.Location = new System.Drawing.Point(12, 388);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(54, 50);
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::Text_game.Properties.Resources.yellow_50x50;
+            this.pictureBox4.Location = new System.Drawing.Point(12, 454);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(54, 51);
+            this.pictureBox4.TabIndex = 4;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::Text_game.Properties.Resources.green_50x50;
+            this.pictureBox5.Location = new System.Drawing.Point(12, 524);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(54, 50);
+            this.pictureBox5.TabIndex = 5;
+            this.pictureBox5.TabStop = false;
+            // 
+            // blueOutputLabel
+            // 
+            this.blueOutputLabel.Font = new System.Drawing.Font("Constantia", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.blueOutputLabel.ForeColor = System.Drawing.Color.White;
+            this.blueOutputLabel.Location = new System.Drawing.Point(72, 327);
+            this.blueOutputLabel.Name = "blueOutputLabel";
+            this.blueOutputLabel.Size = new System.Drawing.Size(396, 50);
+            this.blueOutputLabel.TabIndex = 6;
+            this.blueOutputLabel.Text = "label2";
+            // 
+            // redOutputlabel
+            // 
+            this.redOutputlabel.Font = new System.Drawing.Font("Constantia", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.redOutputlabel.ForeColor = System.Drawing.Color.White;
+            this.redOutputlabel.Location = new System.Drawing.Point(72, 401);
+            this.redOutputlabel.Name = "redOutputlabel";
+            this.redOutputlabel.Size = new System.Drawing.Size(393, 50);
+            this.redOutputlabel.TabIndex = 7;
+            this.redOutputlabel.Text = "label3";
+            // 
+            // yellowOutputlabel
+            // 
+            this.yellowOutputlabel.Font = new System.Drawing.Font("Constantia", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.yellowOutputlabel.ForeColor = System.Drawing.Color.White;
+            this.yellowOutputlabel.Location = new System.Drawing.Point(72, 473);
+            this.yellowOutputlabel.Name = "yellowOutputlabel";
+            this.yellowOutputlabel.Size = new System.Drawing.Size(395, 51);
+            this.yellowOutputlabel.TabIndex = 8;
+            this.yellowOutputlabel.Text = "label4";
+            // 
+            // greenOutputlabel
+            // 
+            this.greenOutputlabel.Font = new System.Drawing.Font("Constantia", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.greenOutputlabel.ForeColor = System.Drawing.Color.White;
+            this.greenOutputlabel.Location = new System.Drawing.Point(72, 537);
+            this.greenOutputlabel.Name = "greenOutputlabel";
+            this.greenOutputlabel.Size = new System.Drawing.Size(392, 50);
+            this.greenOutputlabel.TabIndex = 9;
+            this.greenOutputlabel.Text = "label5";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(480, 596);
+            this.Controls.Add(this.greenOutputlabel);
+            this.Controls.Add(this.yellowOutputlabel);
+            this.Controls.Add(this.redOutputlabel);
+            this.Controls.Add(this.blueOutputLabel);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.eventPicturebox);
+            this.Controls.Add(this.eventOutputlabel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Castle Heist";
+            ((System.ComponentModel.ISupportInitialize)(this.eventPicturebox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label eventOutputlabel;
+        private System.Windows.Forms.PictureBox eventPicturebox;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.Label blueOutputLabel;
+        private System.Windows.Forms.Label redOutputlabel;
+        private System.Windows.Forms.Label yellowOutputlabel;
+        private System.Windows.Forms.Label greenOutputlabel;
     }
 }
 
--- Text game/Form1.cs
+++ Text game/Form1.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿//Created by: Manuel Muncaster
+//Date: November 8, 2016
+//Purpose: Text-based adventure game showcasing switch-case statments and nested if statments
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -13,9 +16,153 @@ namespace Text_game
 
     public partial class Form1 : Form
     {
+        int scene = 0;
+
         public Form1()
         {
             InitializeComponent();
+            eventOutputlabel.Text = "Welcome to Castle Heist! This is a game in which you try and break into a castle to steal as much loot as possible! Good luck!" + Environment.NewLine + Environment.NewLine + "After a three day long walk, you have finally made it to Black Reach Castle. Rumour suggests that the castle's king has gone mad and the castle has been long abandoned. Large sums of gold are said to be hidden within the castle's walls. After closer inspection, you find four ways that you can potentially enter the castle. Which way do you choose?";
+            blueOutputLabel.Text = "Climb up onto the roof.";
+            redOutputlabel.Text = "Try and enter through the front gate.";
+            yellowOutputlabel.Text = "Enter through a sewer drain.";
+            greenOutputlabel.Text = "Sneak through the back gate.";
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.M)
+            {
+                scene = scene++;
+
+            }
+            else if (e.KeyCode == Keys.B)
+            {
+
+            }
+            else if (e.KeyCode == Keys.H)
+            {
+
+            }
+            else if (e.KeyCode == Keys.J)
+            {
+
+            }
+            switch (scene)
+            {
+                case 0:
+                    break;
+                case 1:
+                    eventOutputlabel.Text = "You inspect the wall for a considerable amount of time before finding a way to climb onto the roof safely. Eventually you get to the edge of the roof and pull yourself up. You scan the area and see a medium sized window that you could fit into. Do you try and climb through the window or continue along the roof?";
+                    blueOutputLabel.Text = "Climb into the window.";
+                    redOutputlabel.Text = "Continue walking on the roof.";
+                    yellowOutputlabel.Visible = false;
+                    greenOutputlabel.Visible = false;
+                    break;
+                case 2:
+                    eventOutputlabel.Text = "You decide that the window would not be a good entrance to the castle and continue walking across the roof. Unfortunatly, a archer spots you and puts an arrow into your forehead before you realize he was even there. GAME OVER.";
+                    blueOutputLabel.Text = "Play again?";
+                    redOutputlabel.Text = "Quit!";
+                    yellowOutputlabel.Visible = false;
+                    greenOutputlabel.Visible = false;
+                    break;
+                case 3:
+                    eventOutputlabel.Text = "You climb into the window and find yourself in a forge with a large amount of weapons in pristine condition around you. After gearing up, you see that there are large doors to your left and a long hallway to your right. Which way to do go?";
+                    blueOutputLabel.Text = "Go down the hallway.";
+                    redOutputlabel.Text = "Go near the large doors.";
+                    yellowOutputlabel.Visible = false;
+                    greenOutputlabel.Visible = false;
+                    break;
+                case 4:
+                    eventOutputlabel.Text = "You draw yourself closer to the doors, but something seems quite strange about them. This castle was supposed to be abandoned, but the doors seemed like they were made recently. The door is covered in strange markings and seems to be glowing slightly. Chills run up your spine as you continue to study the door; you feel slighty dizzy and have a slight urge to run. Do you open the doors or continue into a room to the left of the door?";
+                    blueOutputLabel.Text = "Open the strange doors.";
+                    redOutputlabel.Text = "Move away from the strange doors and into the room.";
+                    yellowOutputlabel.Visible = false;
+                    greenOutputlabel.Visible = false;
+                    break;
+                case 5:
+                    eventOutputlabel.Text = "You continue down the hallway away from the large doors. As soon as you turn a corner however, you find a massive barracks full of soldiers! Before you can even figure out how there are so many soldiers in the 'abandoned' castle, a soldier yells at you and a another soldier thrusts his spear in your chest. GAME OVER.";
+                    blueOutputLabel.Text = "Play again?";
+                    redOutputlabel.Text = "Quit!";
+                    yellowOutputlabel.Visible = false;
+                    greenOutputlabel.Visible = false;
+                    break;
+                case 6:
+                    eventOutputlabel.Text = "You trust your instincts and move away from the door quickly into a another room. As soon as you enter the room however, you hear a strange clanking noise, like gears turning. A large being walks out of a door at the far side of the room; some kind of mechanical knight? You panic and try to run out of the room, but the door is locked. You pull out the freshly forged sword you picked up earlier and begin to fight the terrifying figure. You parry a few of it's attacks, but it overwhelms you with its strength and cuts your sword in half. It then screeches a horrible, ear shattering noise and plunges it's blade into your skull and out your lower back. GAME OVER. ";
+                    blueOutputLabel.Text = "Play again?";
+                    redOutputlabel.Text = "Quit!";
+                    yellowOutputlabel.Visible = false;
+                    greenOutputlabel.Visible = false;
+                    break;
+                case 7:
+                    eventOutputlabel.Text = "You fight your instincts and push with all of your strengh to open the doors. You look up and see a breath-taking sight. A large, expansive throne room that has a large glass wall near the back of the room. Behind the wall is a massive pile of gold and gems; as you move closer you notice that the mountain of gold takes of 3/4 of the room. After looking in awe for some time, you look down to see a figure sitting in the throne wearing strange armour that looks very menacing. 'You have somehow broken into my domain, you infidel! Prepare to die!!' The Dread King gets up from his throne and unsheathes his blade; which glows with a strong red light. He jumps 6 feet in the air and lands infront of you. Do you worship the Dread King or fight him?";
+                    blueOutputLabel.Text = "Bow to the Dread King";
+                    redOutputlabel.Text = "Unsheath your blade and prepare for the fight or your life";
+                    yellowOutputlabel.Visible = false;
+                    greenOutputlabel.Visible = false;
+                    break;
+                case 8:
+                    eventOutputlabel.Text = "You decide to bow down on one knee and claim your loyalties to the king. He swiftly chops your head off and with such immense force that your head and body fly out of the window and fall into the castle's moat. GAME OVER.";
+                    blueOutputLabel.Text = "Play again?";
+                    redOutputlabel.Text = "Quit!";
+                    yellowOutputlabel.Visible = false;
+                    greenOutputlabel.Visible = false;
+                    break;
+                case 9:
+                    eventOutputlabel.Text = "You pull out your newly acquired blade and take a defensive stance. The Dread King takes the first swing and you parry back and forth. He nearly chops off your head a few times, but you are reinvigorated by a sudden strength and focus. He slips through your stance and slices your chest, but you don't feel anything. You press on fighting and find yourself gaining the upperhand. You counter a large overhead swing and swing with all of your might; chooping the head of the Dread King off from his body.";
+                    blueOutputLabel.Text = "Climb into the window";
+                    redOutputlabel.Text = "Continue walking on the roof";
+                    yellowOutputlabel.Visible = false;
+                    greenOutputlabel.Visible = false;
+                    break;
+                case 10:
+                    break;
+                case 11:
+                    break;
+                case 12:
+                    break;
+                case 13:
+                    break;
+                case 14:
+                    break;
+                case 15:
+                    break;
+                case 16:
+                    break;
+                case 17:
+                    break;
+                case 18:
+                    break;
+                case 19:
+                    break;
+                case 20:
+                    break;
+                case 21:
+                    break;
+                case 22:
+                    break;
+                case 23:
+                    break;
+                case 24:
+                    break;
+                case 25:
+                    break;
+                case 26:
+                    break;
+                case 27:
+                    break;
+                case 28:
+                    break;
+                case 29:
+                    break;
+                case 30:
+                    break;
+                case 31:
+                    break;
+                case 32:
+                    break;
+                case 33:
+                    break;
+
+            }
         }
     }
 }
